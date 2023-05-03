c93aac460ce6517ba2c6e5706d31d4ae1835a00d
Alin Dragomir <alindragomir@yahoo.com>
2023-05-03 23:17:29 +0300

    new

--- react-rock-paper-scissors/src/App.jsx
+++ react-rock-paper-scissors/src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import styles from 'App.module.css';
+import styles from './App.modules.css';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
       <div className={styles.container}>
-        <div className={styles.titleCtn}>
+        <div className={styles.titleCnt}>
           <h1>ROCK, PAPER, SCISSORS</h1>
           <p>React Game</p>
         </div>
       </div>
-    </>
+    
   )
 }
 
